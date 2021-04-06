d0953796aa260a54bb57b508fdbd796a39b1cd46
Jafet Henrique <jafet.h.fagundes@accenture.com>
2021-04-06 11:07:57 -0300

    adicionando @ENableFeignClients

--- Projeto-Final/Projeto-Final/src/main/java/com/projetofinal/ProjetoFinalApplication.java
+++ Projeto-Final/Projeto-Final/src/main/java/com/projetofinal/ProjetoFinalApplication.java
@@ -2,8 +2,10 @@ package com.projetofinal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ProjetoFinalApplication {
 
 	public static void main(String[] args) {
