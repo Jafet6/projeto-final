37208206ad12549d515a33b54ba90c688f88bee1
Jafet Henrique <jafet.h.fagundes@accenture.com>
2021-04-06 19:05:04 -0300

    adicionando relacionamento ManyToOne

--- Projeto-Final/Projeto-Final/src/main/java/com/projetofinal/repository/BrandRepository.java
+++ Projeto-Final/Projeto-Final/src/main/java/com/projetofinal/repository/BrandRepository.java
@@ -3,5 +3,8 @@ package com.projetofinal.repository;
 import com.projetofinal.domains.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BrandRepository extends JpaRepository<Brand, Long> {
+    Optional<Brand> findByBrand(String brand);
 }
--- Projeto-Final/Projeto-Final/src/main/java/com/projetofinal/repository/CategoryRepository.java
+++ Projeto-Final/Projeto-Final/src/main/java/com/projetofinal/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.projetofinal.repository;
 import com.projetofinal.domains.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByCategory(String category);
 }
